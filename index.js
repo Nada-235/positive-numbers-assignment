edb9f833a2d5ad3509fee294ea36c7942e25dd4d
Nada-235 <nada.alshakiry@gmail.com>
2023-07-11 00:08:51 +0300

    Solved :D

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-class NumberArray {
+/*class NumberArray {
   constructor(numbers) {
     this.numbers = numbers;
   }
@@ -12,11 +12,19 @@ class NumberArray {
     }
     return sum;
   }
-}
+} */
+
+getPositiveSum = (numbers) => numbers.reduce((sum, number) => (number > 0 ? sum + number : sum), 0);
+// Using reduce instead of a for loop and an if condition to check whether the number is positive or not,
+//  and if it is positive, it will be added to the sum."
+
+/* const numbers = [2, -4, 6, -8, 10, -12];
+const numberArray = new NumberArray(numbers);
+const positiveSum = numberArray.getPositiveSum(); */
 
 const numbers = [2, -4, 6, -8, 10, -12];
-const numberArray = new NumberArray(numbers);
-const positiveSum = numberArray.getPositiveSum();
+console.log(`The sum of positive numbers\nin this array [${numbers}] is ${getPositiveSum(numbers)}.`);
+
 
 // Input: [2, -4, 6, -8, 10, -12]
-// Output: 18
+// Output: 18 "Checked" and sorry for being late :)
